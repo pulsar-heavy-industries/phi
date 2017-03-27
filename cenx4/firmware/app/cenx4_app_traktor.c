6ca575037f44476e5c33013f62f4e5f65bb0d3df
Eran Rundstein <e@keves.org>
2017-03-27 01:23:52 -0700

    cenx4_app_traktor improvements

--- cenx4/firmware/app/cenx4_app_traktor.c
+++ cenx4/firmware/app/cenx4_app_traktor.c
@@ -1,5 +1,5 @@
 #include "cenx4_app_traktor.h"
-
+#include "cenx4_conf.h"
 
 const phi_app_desc_t cenx4_app_traktor_desc = {
     .start = cenx4_app_traktor_start,
@@ -15,39 +15,11 @@ const phi_app_desc_t cenx4_app_traktor_desc = {
 void cenx4_app_traktor_start(void * _ctx)
 {
     cenx4_app_traktor_context_t * ctx = (cenx4_app_traktor_context_t *) _ctx;
-	cenx4_ui_t * ui;
 
     chDbgCheck(ctx != NULL);
     memset(ctx, 0, sizeof(*ctx));
 
-	/* Move display 0 into dual pot mode for master/headphone volumes */
-	ui = cenx4_ui_lock(0);
-
-	ui->dispmode = CENX4_UI_DISPMODE_SPLIT_POT;
-
-	memset(&(ui->state), 0, sizeof(ui->state));
-
-	ui->state.split_pot.pots[0].flags =
-			CENX4_UI_DISPMODE_POT_FLAGS_ROUND |
-			CENX4_UI_DISPMODE_POT_FLAGS_FILL |
-			CENX4_UI_DISPMODE_POT_FLAGS_RENDER_VAL |
-			CENX4_UI_DISPMODE_POT_FLAGS_TOP_FONT_AUTO;
-	chsnprintf(ui->state.split_pot.pots[0].text_top, CENX4_UI_MAX_LINE_TEXT_LEN - 1, "Master");
-	ui->state.split_pot.pots[0].val = 20;
-
-	ui->state.split_pot.pots[1].flags = ui->state.split_pot.pots[0].flags;
-	chsnprintf(ui->state.split_pot.pots[1].text_top, CENX4_UI_MAX_LINE_TEXT_LEN - 1, "Headphones");
-	ui->state.split_pot.pots[1].val = 20;
-
-	cenx4_ui_unlock(ui);
-
-	/* Move display 1 into custom mode for displaying master VU meters */
-    ui = cenx4_ui_lock(1);
-    memset(&(ui->state), 0, sizeof(ui->state));
-    ui->state.callback.func = cenx4_app_traktor_render_custom;
-    ui->state.callback.ctx = ctx;
-    ui->dispmode = CENX4_UI_DISPMODE_CALLBACK;
-    cenx4_ui_unlock(ui);
+    cenx4_app_traktor_reconfigure_displays(ctx);
 }
 
 void cenx4_app_traktor_stop(void * ctx)
@@ -60,44 +32,91 @@ void cenx4_app_traktor_tick(void * _ctx)
     cenx4_app_traktor_context_t * ctx = (cenx4_app_traktor_context_t *) _ctx;
 	cenx4_ui_t * ui;
 
-    ui = cenx4_ui_lock(0);
-    ui->state.split_pot.pots[0].val = ctx->master_level;
-    ui->state.split_pot.pots[1].val = ctx->monitor_level;
-    cenx4_ui_unlock(ui);
+	switch (ctx->mode)
+	{
+	case CENX4_APP_TRAKTOR_MODE_DEFAULT:
+		ui = cenx4_ui_lock(0);
+		ui->state.split_pot.pots[0].val = ctx->master_level;
+		ui->state.split_pot.pots[1].val = ctx->monitor_level;
+		cenx4_ui_unlock(ui);
+		break;
+
+	case CENX4_APP_TRAKTOR_MODE_BROWSE:
+		break;
+	}
 }
 
-void cenx4_app_traktor_encoder_event(void * ctx, uint8_t node_id, uint8_t encoder_num, int8_t val_change)
+void cenx4_app_traktor_encoder_event(void * _ctx, uint8_t node_id, uint8_t encoder_num, int8_t val_change)
 {
+    cenx4_app_traktor_context_t * ctx = (cenx4_app_traktor_context_t *) _ctx;
+    phi_midi_pkt_t pkt;
+
+    const uint8_t encoder_to_cc_map[][CENX4_MAX_POTS] = {
+    	// CENX4_APP_TRAKTOR_MODE_DEFAULT
+    	{
+    		CENX4_APP_TRAKTOR_MIDI_CC_MASTER_LEVEL,
+    		CENX4_APP_TRAKTOR_MIDI_CC_MONITOR_LEVEL,
+    		CENX4_APP_TRAKTOR_MIDI_CC_UNUSED,
+    		CENX4_APP_TRAKTOR_MIDI_CC_UNUSED,
+    	},
+		// CENX4_APP_TRAKTOR_MODE_BROWSE
+		{
+			CENX4_APP_TRAKTOR_MIDI_CC_BROWSE_SELECT_PAGE,
+			CENX4_APP_TRAKTOR_MIDI_CC_BROWSE_SELECT,
+			CENX4_APP_TRAKTOR_MIDI_CC_UNUSED,
+			CENX4_APP_TRAKTOR_MIDI_CC_BROWSE_TREE_SELECT,
+		},
+    };
+
+    chDbgCheck(ctx->mode < PHI_ARRLEN(encoder_to_cc_map));
+
+	if (node_id == 0)
+	{
+		pkt.chn = CENX4_APP_TRAKTOR_MIDI_CH_MASTER;
+		pkt.event = 0xB; // Control Change
+		pkt.val1 = encoder_to_cc_map[ctx->mode][encoder_num];
+		pkt.val2 = 64 + val_change; // "Relative Binary Offset". Positive offsets are sent as offset plus 64 and negative offsets are sent as 64 minus offset
+		phi_midi_tx_pkt(PHI_MIDI_PORT_USB, &pkt);
+	}
+}
+
+void cenx4_app_traktor_btn_event(void * _ctx, uint8_t node_id, uint8_t btn_num, phi_btn_event_t event, uint32_t param)
+{
+    cenx4_app_traktor_context_t * ctx = (cenx4_app_traktor_context_t *) _ctx;
     phi_midi_pkt_t pkt;
 
 	if (node_id == 0)
 	{
-		switch (encoder_num)
+		switch (btn_num)
 		{
-		// Master
-		case 0:
-		    pkt.chn = CENX4_APP_TRAKTOR_MIDI_CH_MASTER;
-		    pkt.event = 0xB; // Control Change
-		    pkt.val1 = CENX4_APP_TRAKTOR_MIDI_CC_MASTER_LEVEL;
-		    pkt.val2 = 64 + val_change; // "Relative Binary Offset". Positive offsets are sent as offset plus 64 and negative offsets are sent as 64 minus offset
-		    phi_midi_tx_pkt(PHI_MIDI_PORT_USB, &pkt);
-			break;
-
-		// Monitor
+		// Browse mode (toggle)
 		case 1:
-		    pkt.chn = CENX4_APP_TRAKTOR_MIDI_CH_MASTER;
-		    pkt.event = 0xB; // Control Change
-		    pkt.val1 = CENX4_APP_TRAKTOR_MIDI_CC_MONITOR_LEVEL;
-		    pkt.val2 = 64 + val_change; // "Relative Binary Offset". Positive offsets are sent as offset plus 64 and negative offsets are sent as 64 minus offset
-		    phi_midi_tx_pkt(PHI_MIDI_PORT_USB, &pkt);
-			break;
+			if (event == PHI_BTN_EVENT_PRESSED)
+			{
+				pkt.chn = CENX4_APP_TRAKTOR_MIDI_CH_MASTER;
+				pkt.event = 0xB; // Control Change
+				pkt.val1 = CENX4_APP_TRAKTOR_MIDI_CC_BROWSE_MODE;
+				pkt.val2 = 1;
+				phi_midi_tx_pkt(PHI_MIDI_PORT_USB, &pkt);
+			}
+		    break;
+
+		case 3:
+			switch (ctx->mode)
+			{
+			case CENX4_APP_TRAKTOR_MODE_DEFAULT:
+				break;
+
+			case CENX4_APP_TRAKTOR_MODE_BROWSE:
+				pkt.chn = CENX4_APP_TRAKTOR_MIDI_CH_MASTER;
+				pkt.event = 0xB; // Control Change
+				pkt.val1 = CENX4_APP_TRAKTOR_MIDI_CC_BROWSE_TREE_EXPAND;
+				pkt.val2 = 1;
+				phi_midi_tx_pkt(PHI_MIDI_PORT_USB, &pkt);
+				break;
+			}
 		}
 	}
-}
-
-void cenx4_app_traktor_btn_event(void * ctx, uint8_t node_id, uint8_t btn_num, phi_btn_event_t event, uint32_t param)
-{
-
 }
 
 void cenx4_app_traktor_midi_cc(void * _ctx, phi_midi_port_t port, uint8_t ch, uint8_t cc, uint8_t val)
@@ -124,9 +143,70 @@ void cenx4_app_traktor_midi_cc(void * _ctx, phi_midi_port_t port, uint8_t ch, ui
     	case CENX4_APP_TRAKTOR_MIDI_CC_MONITOR_LEVEL:
     		ctx->monitor_level = val << 1;
     		break;
+
+    	case CENX4_APP_TRAKTOR_MIDI_CC_BROWSE_MODE:
+    		ctx->mode = val ? CENX4_APP_TRAKTOR_MODE_BROWSE : CENX4_APP_TRAKTOR_MODE_DEFAULT;
+    		cenx4_app_traktor_reconfigure_displays(ctx);
+    		break;
     	}
     	break;
     }
+}
+
+void cenx4_app_traktor_reconfigure_displays(cenx4_app_traktor_context_t * ctx)
+{
+	cenx4_ui_t * ui;
+
+	/* Move display 0 */
+	ui = cenx4_ui_lock(0);
+
+	switch (ctx->mode)
+	{
+	case CENX4_APP_TRAKTOR_MODE_DEFAULT:
+		ui->dispmode = CENX4_UI_DISPMODE_SPLIT_POT;
+
+		memset(&(ui->state), 0, sizeof(ui->state));
+
+		ui->state.split_pot.pots[0].flags =
+				CENX4_UI_DISPMODE_POT_FLAGS_ROUND |
+				CENX4_UI_DISPMODE_POT_FLAGS_FILL |
+				CENX4_UI_DISPMODE_POT_FLAGS_RENDER_VAL |
+				CENX4_UI_DISPMODE_POT_FLAGS_TOP_FONT_AUTO;
+		chsnprintf(ui->state.split_pot.pots[0].text_top, CENX4_UI_MAX_LINE_TEXT_LEN - 1, "Master");
+		ui->state.split_pot.pots[0].val = ctx->master_level;
+
+		ui->state.split_pot.pots[1].flags = ui->state.split_pot.pots[0].flags;
+		chsnprintf(ui->state.split_pot.pots[1].text_top, CENX4_UI_MAX_LINE_TEXT_LEN - 1, "Headphones");
+		ui->state.split_pot.pots[1].val = ctx->monitor_level;
+		break;
+
+	case CENX4_APP_TRAKTOR_MODE_BROWSE:
+		ui->state.callback.func = cenx4_app_traktor_render_browse;
+		ui->state.callback.ctx = ctx;
+		ui->dispmode = CENX4_UI_DISPMODE_CALLBACK;
+		break;
+
+	default:
+		chDbgCheck(FALSE);
+	}
+
+	cenx4_ui_unlock(ui);
+
+	/* Move display 1 into custom mode for displaying master VU meters */
+	ui = cenx4_ui_lock(1);
+	memset(&(ui->state), 0, sizeof(ui->state));
+	ui->state.callback.func = cenx4_app_traktor_render_custom_1;
+	ui->state.callback.ctx = ctx;
+	ui->dispmode = CENX4_UI_DISPMODE_CALLBACK;
+	cenx4_ui_unlock(ui);
+}
+
+void cenx4_app_traktor_render_browse(cenx4_ui_t * ui, void * _ctx)
+{
+    cenx4_app_traktor_context_t * ctx = (cenx4_app_traktor_context_t *) _ctx;
+
+    gdispGClear(ui->g, Black);
+	cenx4_ui_text(ui, 0, 0, ui->w, 1, justifyCenter, "Browse");
 }
 
 void vertical_vu_helper(cenx4_ui_t * ui, uint8_t n, uint8_t val)
@@ -157,7 +237,7 @@ void vertical_vu_helper(cenx4_ui_t * ui, uint8_t n, uint8_t val)
 	gdispGFillArea(ui->g, vu_x + vu_bar_x_offset, vu_y + vu_bar_y_offset + (vu_bar_max_height - vu_bar_height), vu_bar_width, vu_bar_height, White);
 }
 
-void cenx4_app_traktor_render_custom(cenx4_ui_t * ui, void * _ctx)
+void cenx4_app_traktor_render_custom_1(cenx4_ui_t * ui, void * _ctx)
 {
     cenx4_app_traktor_context_t * ctx = (cenx4_app_traktor_context_t *) _ctx;
 
--- cenx4/firmware/app/cenx4_app_traktor.h
+++ cenx4/firmware/app/cenx4_app_traktor.h
@@ -8,17 +8,32 @@
 
 #define CENX4_APP_TRAKTOR_MIDI_CH_MASTER	0
 
-#define CENX4_APP_TRAKTOR_MIDI_CC_MASTER_VU_LEFT	1
-#define CENX4_APP_TRAKTOR_MIDI_CC_MASTER_VU_RIGHT   2
-#define CENX4_APP_TRAKTOR_MIDI_CC_MASTER_LEVEL		3
-#define CENX4_APP_TRAKTOR_MIDI_CC_MONITOR_LEVEL		4
+#define CENX4_APP_TRAKTOR_MIDI_CC_MASTER_VU_LEFT			1
+#define CENX4_APP_TRAKTOR_MIDI_CC_MASTER_VU_RIGHT   		2
+#define CENX4_APP_TRAKTOR_MIDI_CC_MASTER_LEVEL				3
+#define CENX4_APP_TRAKTOR_MIDI_CC_MONITOR_LEVEL				4
+#define CENX4_APP_TRAKTOR_MIDI_CC_BROWSE_MODE				5
+#define CENX4_APP_TRAKTOR_MIDI_CC_BROWSE_SELECT				6
+#define CENX4_APP_TRAKTOR_MIDI_CC_BROWSE_SELECT_PAGE		7
+#define CENX4_APP_TRAKTOR_MIDI_CC_BROWSE_TREE_SELECT		8
+#define CENX4_APP_TRAKTOR_MIDI_CC_BROWSE_TREE_EXPAND		9
+
+#define CENX4_APP_TRAKTOR_MIDI_CC_UNUSED			63
+
+typedef enum
+{
+	CENX4_APP_TRAKTOR_MODE_DEFAULT = 0,
+	CENX4_APP_TRAKTOR_MODE_BROWSE,
+} cenx4_app_traktor_mode_t;
 
 extern const phi_app_desc_t cenx4_app_traktor_desc;
 
 typedef struct cenx4_app_traktor_context_s
 {
+	cenx4_app_traktor_mode_t mode;
 	uint8_t master_l, master_r;
 	uint8_t master_level, monitor_level;
+	bool_t browse_mode;
 } cenx4_app_traktor_context_t;
 
 
@@ -28,6 +43,8 @@ void cenx4_app_traktor_tick(void * ctx);
 void cenx4_app_traktor_encoder_event(void * ctx, uint8_t node_id, uint8_t encoder_num, int8_t val_change);
 void cenx4_app_traktor_btn_event(void * ctx, uint8_t node_id, uint8_t btn_num, phi_btn_event_t event, uint32_t param);
 void cenx4_app_traktor_midi_cc(void * ctx, phi_midi_port_t port, uint8_t ch, uint8_t cc, uint8_t val);
-void cenx4_app_traktor_render_custom(cenx4_ui_t * ui, void * _ctx);
+void cenx4_app_traktor_reconfigure_displays(cenx4_app_traktor_context_t * ctx);
+void cenx4_app_traktor_render_browse(cenx4_ui_t * ui, void * _ctx);
+void cenx4_app_traktor_render_custom_1(cenx4_ui_t * ui, void * _ctx);
 
 #endif /* CENX4_APP_TRAKTOR_H_ */
