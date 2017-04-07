619319e31857db849175af87241eaed9954ded61
Eran Rundstein <e@keves.org>
2017-04-07 01:22:43 -0700

    update TODO

--- cenx4/firmware/app/cenx4_conf.h
+++ cenx4/firmware/app/cenx4_conf.h
@@ -7,7 +7,7 @@
  * CENX4 Specific stuff
  *****************************************************************************/
 
-#define CENX4_HW_SW_VER 0x0101
+#define CENX4_HW_SW_VER 0x0102
 #define CENX4_DEV_ID     PHI_DEV_ID('C', 'N', 'X', '4')
 #define CENX4_MAX_POTS   ((PHI_CAN_AUTO_ID_ALLOCATOR_MAX_DEVS + 1) * 4) // 4 pots per module and 1 extra module for the master
 
